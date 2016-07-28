a1c6db6cccf8acd3cee18edbe9a13fc6dffe76cc
Juliette Coia <jcoiapdx@gmail.com>
2016-07-28 13:27:22 -0700

    app with maps- does not run

---
+++ essai-carte/app/src/main/java/com/example/juliettecoia/essai_carte/MenuConfirmActivity.java
@@ -0,0 +1,9 @@
+package com.example.juliettecoia.essai_carte;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by juliettecoia on 7/28/16.
+ */
+public class MenuConfirmActivity extends FragmentActivity{
+}
