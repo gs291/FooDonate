b50ee651cf97fc0103113454dab077632f863c52
marleybennett98@gmail.com <marleybennett@Marleys-MacBook-Air.local>
2016-08-01 10:29:51 -0700

    Up to date version of Food Donate app

---
+++ app/src/main/java/com/example/juliettecoia/essai_carte/MissionActivity.java
@@ -0,0 +1,16 @@
+package com.example.juliettecoia.essai_carte;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by juliettecoia on 7/29/16.
+ */
+public class MissionActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mission_page);
+    }
+}
